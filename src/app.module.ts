cf5168374c81ea813f6804062cc0a051778f3a83
sambare <boubasambare@yahoo.it>
2021-02-12 02:37:43 +0100

    style: :construction:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { UnsplastModule } from './unsplast.module';
+import { UnsplastController } from './unsplast.controller';
 import { WeatherService } from './weather.service';
 import { WeatherController } from './weather.controller';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [WeatherController],
+  imports: [UnsplastModule, ConfigModule.forRoot()],
+  controllers: [UnsplastController, WeatherController],
   providers: [WeatherService],
 })
 export class AppModule {}
---
+++ src/unsplast.module.ts
@@ -0,0 +1,9 @@
+import { UnsplastService } from './unsplast.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [UnsplastService],
+})
+export class UnsplastModule {}
